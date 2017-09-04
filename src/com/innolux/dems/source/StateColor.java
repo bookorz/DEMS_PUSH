6737f57fa0e9b5b87ab0f8fbf9724559a10fb7a5
book <book.huang@innolux.com>
2017-09-04 14:35:52 +0800

    優化

---
+++ src/com/innolux/dems/source/StateColor.java
@@ -0,0 +1,68 @@
+package com.innolux.dems.source;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Vector;
+
+import org.apache.log4j.Logger;
+
+import com.innolux.dems.DBConnector;
+import com.innolux.dems.GlobleVar;
+public class StateColor {
+	private Logger logger = Logger.getLogger(this.getClass());
+	private DBConnector DEMS = GlobleVar.DEMS;
+
+	private Tools tools = new Tools();
+	public String Color = "";
+	public String State = "";
+	public String Type = "";
+	
+	public Vector<StateColor> Get(){
+		Vector<StateColor> result = new Vector<StateColor>();
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		String SQL = "";
+		try {
+			conn = DEMS.getConnection();
+			stmt = conn.createStatement();
+			
+			
+			SQL = "select * from dems_state_color t";
+				
+			logger.debug(SQL);
+
+			rs = stmt.executeQuery(SQL);
+
+			while (rs.next()) {
+				StateColor each = new StateColor();
+				each.Color = rs.getString("Color");
+				each.Type = rs.getString("Type");
+				each.State = rs.getString("State");
+				result.add(each);
+			}
+		} catch (Exception e) {
+			logger.error(tools.StackTrace2String(e));
+
+		} finally {
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					logger.error(tools.StackTrace2String(e));
+				}
+			}
+			if (conn != null) {
+				try {
+
+					DEMS.closeConnection(conn);
+
+				} catch (SQLException e) {
+					logger.error(tools.StackTrace2String(e));
+				}
+			}
+		}
+		return result;
+	}
+}
